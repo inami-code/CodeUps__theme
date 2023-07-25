08edcdc2396b917a58a53f3b418e3fe41a8a6c99
inami-code <inami.design@gmail.com>
2023-07-25 19:53:21 +0900

    2nd commit(0725)

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,17 @@
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
 
+  //ドロワーメニュー
+  $(".js-hamburger").click(function () {
+    if ($(".js-hamburger").hasClass("is-active")) {
+      $(".js-hamburger").removeClass("is-active");
+      $(".js-sp-nav").fadeOut(300);
+      $("header").removeClass("is-active");
+    } else {
+      $(".js-hamburger").addClass("is-active");
+      $(".js-sp-nav").fadeIn(300);
+      $(".header").addClass("is-active");
+    }
+  });
+
 });
