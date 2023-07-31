7e325483b9b1bef8109a90fc5ae554c1851d37ee
inami-code <inami.design@gmail.com>
2023-07-31 18:59:53 +0900

    0731ピクパ

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,13 @@
 jQuery(function ($) {
   // この中であればWordpressでも「$」が使用可能になる
 
+  //mv
+  $(window).on("load", function () {
+    $(".mv__swiper").addClass("js-show");
+    $(".mv-movie__title").addClass("is-none");
+    $(".mv-movie__subtitle").addClass("is-none");
+  });
+
   //ドロワーメニュー
   $(".js-hamburger").click(function () {
     if ($(".js-hamburger").hasClass("is-active")) {
@@ -28,13 +35,12 @@ jQuery(function ($) {
     loop: true,
     loopAdditionalSlides: 4,
     spaceBetween: 24,
-    width: 289,
+    width: 280,
     breakpoints: {
-
       768: {
         spaceBetween: 40,
         width: 333,
-      }
+      },
     },
     grabCursor: true,
     navigation: {
@@ -43,4 +49,128 @@ jQuery(function ($) {
     },
   });
 
+  //画像アニメーション
+
+  //要素の取得とスピードの設定
+  var box = $(".info__image"),
+    speed = 700;
+
+  //.colorboxの付いた全ての要素に対して下記の処理を行う
+  box.each(function () {
+    $(this).append('<div class="js-image-color"></div>');
+    var color = $(this).find($(".js-image-color")),
+      image = $(this).find("img");
+    var counter = 0;
+
+    image.css("opacity", "0");
+    color.css("width", "0%");
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on("inview", function () {
+      if (counter == 0) {
+        $(this)
+          .delay(200)
+          .animate({ width: "100%" }, speed, function () {
+            image.css("opacity", "1");
+            $(this).css({ left: "0", right: "auto" });
+            $(this).animate({ width: "0%" }, speed);
+          });
+        counter = 1;
+      }
+    });
+  });
+
+  //要素の取得とスピードの設定
+  var box = $(".voice-card__image"),
+    speed = 700;
+
+  //.js-image-color imgの付いた全ての要素に対して下記の処理を行う
+  box.each(function () {
+    $(this).append('<div class="js-image-color"></div>');
+    var color = $(this).find($(".js-image-color")),
+      image = $(this).find("img");
+    var counter = 0;
+
+    image.css("opacity", "0");
+    color.css("width", "0%");
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on("inview", function () {
+      if (counter == 0) {
+        $(this)
+          .delay(200)
+          .animate({ width: "100%" }, speed, function () {
+            image.css("opacity", "1");
+            $(this).css({ left: "0", right: "auto" });
+            $(this).animate({ width: "0%" }, speed);
+          });
+        counter = 1;
+      }
+    });
+  });
+
+  //要素の取得とスピードの設定
+  var box = $(".price__image"),
+    speed = 700;
+
+  //.js-image-color pictureの付いた全ての要素に対して下記の処理を行う
+  box.each(function () {
+    $(this).append('<div class="js-image-color"></div>');
+    var color = $(this).find($(".js-image-color")),
+      image = $(this).find("picture");
+    var counter = 0;
+
+    image.css("opacity", "0");
+    color.css("width", "0%");
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on("inview", function () {
+      if (counter == 0) {
+        $(this)
+          .delay(200)
+          .animate({ width: "100%" }, speed, function () {
+            image.css("opacity", "1");
+            $(this).css({ left: "0", right: "auto" });
+            $(this).animate({ width: "0%" }, speed);
+          });
+        counter = 1;
+      }
+    });
+  });
+
+  // ページトップに戻るボタン
+
+  $(document).ready(function () {
+    const pageTop = $(".to-top");
+    pageTop.hide();
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        pageTop.fadeIn();
+      } else {
+        pageTop.fadeOut();
+      }
+    });
+    pageTop.click(function () {
+      $("body,html").animate(
+        {
+          scrollTop: 0,
+        },
+        500
+      );
+      return false;
+    });
+  
+    $(window).on("scroll", function () {
+      const scrollHeight = $(document).height();
+      const scrollPosition = $(window).height() + $(window).scrollTop();
+      const footHeight = $("footer").innerHeight();
+      const offset = $(window).width() < 768 ? 15 : 20; // スマートフォン表示時は15px、それ以外は20px
+  
+      if (scrollHeight - scrollPosition <= footHeight) {
+        // 下側から（フッターの高さ + offset）に配置
+        $(".to-top").css({ position: "absolute", bottom: footHeight + offset });
+      } else {
+        // 下側からoffsetの位置に配置
+        $(".to-top").css({ position: "fixed", bottom: offset + "px" });
+      }
+    });
+  });
+  
 });
